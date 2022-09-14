831f00ad7c0438c094228239b61b4145d0be8a0c
nayem9b <razwanniam1@gmail.com>
2022-09-14 20:53:45 +0600

    Countdown as a round number fixed

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const start = () => {
     countdownOverlay.innerHTML = '<h1>${count}</h1>';
 
     // finished timer
-    if (count == 3) {
+    if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
@@ -135,5 +135,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.round(timeSpent) : 0} seconds`;
 }, 1000);
