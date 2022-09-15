294725489c3ee1058bf0567e0c7f9c8580f1cff0
nayem9b <razwanniam1@gmail.com>
2022-09-15 16:21:19 +0600

    blog button fixed

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -90,7 +90,7 @@ const gameOver = () => {
     <button onclick="closeModal()">Close</button>
   `;
 
-  addHistory(questionText, timeTaken, errorCount);
+  addHistory(questionText,Math.round(timeTaken), errorCount);
 
   // restart everything
   startTime = null;
